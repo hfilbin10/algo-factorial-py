bed8c326b5ae14613b9a266f2eee60b67374c6ec
Halsey Filbin <halsey.t.filbin@gmail.com>
2023-10-05 20:25:30 -0500

    Solved using reduce and lambda

--- factorial.py
+++ factorial.py
@@ -1,3 +1,10 @@
+from functools import reduce
+
 def factorial(num):
-	# your code here
-	pass
+	if num == 0:
+		return 1
+	
+	numbers = list(range(1, num + 1)) # create list from 1 n
+
+	answer = reduce(lambda a, b: a * b, numbers) # reduce() multiplies all numbers in list
+	return answer
